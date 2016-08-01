0dd50e35eb7047da19b8fc31cdb2f2a5b77d6ee2
Hung Dinh <dinhhungvn@gmail.com>
2016-08-02 00:13:01 +0700

    update README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+When i code, I always have to remember a lot of different statements, and it's really time-consuming and tiring. Especially when you are working with a new PC.
+That's why I created the `bb`
+
+## Usage is simple:
+### NEED FOR DEPLOY WITH DOCKER
+    bb start
+
+This command will auto install:
+* git
+* docker
+* docker-compose
+
+
+## I'm still trying to supplement and perfect `bb`
+ If you are interested, please build it with me
--- lib/func.sh
+++ lib/func.sh
@@ -13,28 +13,33 @@ start(){
   sudo apt-get update
   # Instal git
   if hash git 2>/dev/null; then
-    printf "Git has been installed!"
+    printf "Git has been installed!\n"
   else
     sudo apt-get install git && git config
   fi
   # Install Docker
-  if has docker 2>/dev/null; then
+  if hash docker 2>/dev/null; then
     printf "Docker has been installed\n"
   else
-    printf "=====================INSTALL DOCKER==============="
+    printf "\n=====================INSTALL DOCKER===============\n"
     wget -qO- https://get.docker.com/ | sh
   fi
   # Install Docker Compose
   # First, install python-pip as prerequisite:
-  if docker-compose 2>/dev/null; then
+  if hash docker-compose 2>/dev/null; then
     printf "Docker-compose has been installed\n"
   else
-    printf "===================INSTALL DOCKER-COMPOSE==========="
+    printf "\n===================INSTALL DOCKER-COMPOSE===========\n"
     sudo apt-get -y install python-pip
     # After you can instal docker compose
     sudo pip install docker-compose
   fi
-
+  # Install Pbclip
+  if hash xclip 2>/dev/null; then
+    printf "\nXclip has been installed!\n"
+  else
+    sudo apt-get install -y xclip
+  fi
 
 
 
