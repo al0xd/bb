ba8568fd90470f398a4161af424a0ef05382e67c
Hung Dinh <dinhhungvn@gmail.com>
2016-08-01 17:57:17 +0700

    update install validate packages

--- lib/func.sh
+++ lib/func.sh
@@ -18,13 +18,22 @@ start(){
     sudo apt-get install git && git config
   fi
   # Install Docker
-  wget -qO- https://get.docker.com/ | sh
+  if has docker 2>/dev/null; then
+    printf "Docker has been installed\n"
+  else
+    printf "=====================INSTALL DOCKER==============="
+    wget -qO- https://get.docker.com/ | sh
+  fi
   # Install Docker Compose
   # First, install python-pip as prerequisite:
-  sudo apt-get -y install python-pip
-  # After you can instal docker compose
-  sudo pip install docker-compose
-
+  if docker-compose 2>/dev/null; then
+    printf "Docker-compose has been installed\n"
+  else
+    printf "===================INSTALL DOCKER-COMPOSE==========="
+    sudo apt-get -y install python-pip
+    # After you can instal docker compose
+    sudo pip install docker-compose
+  fi
 
 
 
